25d4ddab6eecaf112e8de1a509b7779b88256773
nijatmammadovv <tu7q78xlv@code.edu.az>
2022-03-30 12:29:24 +0400

    Add project files.

---
+++ Task-30.03.2022/Models/CustomList.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_30._03._2022.Models
+{
+    class CustomList <T>
+    {
+        private T[] array;
+        private int Capacity;
+        private int _Count;
+
+        public CustomList()
+        {
+            array = new T[0];
+        }
+        public T this [int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+        public void Add(T number)
+        {
+
+        }
+
+
+        public void Clear()
+        {
+            array = new T[Capacity];
+            _Count = 0;
+        }
+        public bool Exist(T Number)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array.Equals(Number))
+                    return true;
+            }
+            return false;
+        }
+        /*
+        public void Remove(T Number)
+        {
+
+        }
+        */
+        public void Reverse()
+        {
+            Array.Reverse(array);
+        }
+        public int IndexOf(T Number)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i].Equals(Number))
+                    return i;
+            }
+            return -1;
+        }
+        public int LastIndexOf(T Number)
+        {
+            for(int i = array.Length - 1; i > 0; i--)
+            {
+                if (array[i].Equals(Number))
+                  return i;
+            }
+            return -1;
+        }
+        public void ShowInfo()
+        {
+            foreach(var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Task-30.03.2022/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Task_30._03._2022.Models;
+using System.Collections.Generic;
+
+namespace Task_30._03._2022
+{
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList <int> list = new CustomList<int>();
+            list.Add(5);
+            list.Add(10);
+            list.Add(15);
+            list.Add(20);
+            list.Add(25);
+            list.Add(30);
+            list.Add(35);
+            list.Add(40);
+            list.Add(45);
+            list.Add(50);
+
+
+            Console.WriteLine(list.IndexOf(3));
+            Console.WriteLine("-------------------");
+            Console.WriteLine(list.LastIndexOf(4));
+            Console.WriteLine("--------------------");
+
+
+        }
+    }
+}
